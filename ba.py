154780bd7daa28e799c935c6b191e2ca8bafbcab
Surya <suryaprakashna970@gamil.com>
2026-05-23 23:50:34 +0530

    Updated project files

--- ba.py
+++ ba.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import pandas as pd
+
+df = pd.read_csv("surya.csv")
 
 app = Flask(__name__)
 
@@ -6,5 +9,20 @@ app = Flask(__name__)
 def home():
     return render_template('calcualte.html')
 
-if __name__ == '__main__':
+
+@app.route('/post', methods=["POST"])
+def post_data():
+
+    email = request.form.get("username")
+    password = request.form.get("password")
+
+    login = (df["email"] == email) & (df["password"] == password)
+
+    if login.any(): # student login 
+        return render_template("succecs_massage.html")
+    else:
+        return "Invalid Login"
+
+
+if __name__ == "__main__":
     app.run(debug=True)
